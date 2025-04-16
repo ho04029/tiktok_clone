3c570e2e836ffbf5b11d40d6184bc59ebdd26c59
seokgyeong Lee <ho04029@naver.com>
2025-04-16 11:23:50 +0900

    feat: email, password screen

--- lib/features/authentication/password_screen.dart
+++ lib/features/authentication/password_screen.dart
@@ -1,45 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/password_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/from_button.dart';
 
-class EmailScreen extends StatefulWidget {
-  const EmailScreen({super.key});
+class PasswordScreen extends StatefulWidget {
+  const PasswordScreen({super.key});
 
   @override
-  State<EmailScreen> createState() => _EmailScreen();
+  State<PasswordScreen> createState() => _PasswordScreen();
 }
 
-class _EmailScreen extends State<EmailScreen> {
-  final TextEditingController _emailController = TextEditingController();
+class _PasswordScreen extends State<PasswordScreen> {
+  final TextEditingController _passwordController = TextEditingController();
 
-  String _email = "";
+  String _password = "";
 
   @override
   void initState() {
     super.initState();
 
-    _emailController.addListener(() {
+    _passwordController.addListener(() {
       setState(() {
-        _email = _emailController.text;
+        _password = _passwordController.text;
       });
     });
   }
 
   @override
   void dispose() {
-    _emailController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
-  String? _isEmailValid() {
-    if (_email.isEmpty) return null;
+  String? _isPasswordValid() {
+    if (_password.isEmpty) return null;
     //이메일 정규 표현식
     final regExp = RegExp(
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
     );
-    if (!regExp.hasMatch(_email)) {
+    if (!regExp.hasMatch(_password)) {
       return "Email Not valid";
     }
     return null;
@@ -52,7 +52,7 @@ class _EmailScreen extends State<EmailScreen> {
 
   void _onSubmit() {
     //이메일이 적혀저 있지 않거나 유효하지 않으면 아무 동작도 하지 않음
-    if (_email.isEmpty || _isEmailValid() != null) return;
+    if (_password.isEmpty || _isPasswordValid() != null) return;
     //유효하면 비밀번호 입력 페이지로 넘겨줌
     Navigator.push(
       context,
@@ -74,7 +74,7 @@ class _EmailScreen extends State<EmailScreen> {
             children: [
               Gaps.v40,
               Text(
-                "What is your email?",
+                "Password",
                 style: TextStyle(
                   fontSize: Sizes.size24,
                   fontWeight: FontWeight.w700,
@@ -82,13 +82,20 @@ class _EmailScreen extends State<EmailScreen> {
               ),
               Gaps.v16,
               TextField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
+                controller: _passwordController,
                 autocorrect: false,
                 onEditingComplete: _onSubmit,
                 decoration: InputDecoration(
-                  hintText: "Email",
-                  errorText: _isEmailValid(),
+                  suffix: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      FaIcon(FontAwesomeIcons.circleXmark),
+                      Gaps.h5,
+                      FaIcon(FontAwesomeIcons.eye),
+                    ],
+                  ),
+                  hintText: "Password",
+                  errorText: _isPasswordValid(),
                   enabledBorder: UnderlineInputBorder(
                     borderSide: BorderSide(color: Colors.grey),
                   ),
@@ -102,7 +109,7 @@ class _EmailScreen extends State<EmailScreen> {
               GestureDetector(
                 onTap: _onSubmit,
                 child: FormButton(
-                  disabled: _email.isEmpty || _isEmailValid() != null,
+                  disabled: _password.isEmpty || _isPasswordValid() != null,
                 ),
               ),
             ],
