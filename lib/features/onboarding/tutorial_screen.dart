afd142733e3ba557c16529bf4b014daac6ecccb6
seokgyeong Lee <ho04029@naver.com>
2025-10-27 17:49:52 +0900

    tutorial 페이지

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -2,34 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
-// Padding(
-//     padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-//     child: Column(
-//       crossAxisAlignment: CrossAxisAlignment.start,
-//       children: [
-//         Gaps.v52,
-//         Text(
-//           "Enjoy the ride",
-//           style: TextStyle(
-//             fontSize: Sizes.size40,
-//             fontWeight: FontWeight.bold,
-//           ),
-//         ),
-//         Gaps.v16,
-//         Text(
-//           "Videos are personalized for you based on what you watch, like, and share.",
-//           style: TextStyle(fontSize: Sizes.size20),
-//         ),
-//       ],
-//     ),
-//   ),
+enum Direction { right, left }
 
-class TutorialScreen extends StatelessWidget {
+class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails detail) {}
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
       child: Scaffold(
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
@@ -38,6 +41,7 @@ class TutorialScreen extends StatelessWidget {
               firstChild: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: const [
+                  Gaps.v80,
                   Text(
                     "Watch cool videos!",
                     style: TextStyle(
@@ -55,6 +59,7 @@ class TutorialScreen extends StatelessWidget {
               secondChild: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: const [
+                  Gaps.v80,
                   Text(
                     "Follow the rules",
                     style: TextStyle(
