ff32ee85837c377388af77d784574612337c6b29
seokgyeong Lee <ho04029@naver.com>
2025-10-31 13:46:39 +0900

    feat: 메인 화면

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -14,14 +14,6 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIdx = 0;
 
-  final screens = [
-    const Center(child: Text("Home")),
-    const Center(child: Text("Search")),
-    const Center(child: Text("Search")),
-    const Center(child: Text("Search")),
-    const Center(child: Text("Search")),
-  ];
-
   void _onTap(int index) {
     setState(() {
       _selectedIdx = index;
@@ -31,51 +23,76 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          Offstage(offstage: _selectedIdx != 0, child: Column()),
+          Offstage(offstage: _selectedIdx != 1, child: Column()),
+          Offstage(offstage: _selectedIdx != 1, child: Column()),
+          Offstage(offstage: _selectedIdx != 1, child: Column()),
+          Offstage(offstage: _selectedIdx != 1, child: Column()),
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         padding: EdgeInsets.all(Sizes.size12),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
+            NavTab(
+              text: "Home",
+              isSelected: _selectedIdx == 0,
+              icon: FontAwesomeIcons.house,
+              selectedIcon: FontAwesomeIcons.house,
+              onTab: () => _onTap(0),
+            ),
+            NavTab(
+              text: "Discover",
+              isSelected: _selectedIdx == 1,
+              icon: FontAwesomeIcons.compass,
+              selectedIcon: FontAwesomeIcons.solidCompass,
+              onTab: () => _onTap(1),
+            ),
+            Gaps.h24,
+            Stack(
               children: [
-                FaIcon(FontAwesomeIcons.house, color: Colors.white),
-                Gaps.v5,
-                Text("Home", style: TextStyle(color: Colors.white)),
+                Positioned(
+                  child: Container(
+                    height: 30,
+                    width: 25,
+                    padding: EdgeInsets.symmetric(horizontal: Sizes.size8),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(Sizes.size11),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  child: Container(
+                    height: 30,
+                    width: 25,
+                    padding: EdgeInsets.symmetric(horizontal: Sizes.size8),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(Sizes.size11),
+                    ),
+                  ),
+                ),
               ],
             ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                FaIcon(FontAwesomeIcons.house, color: Colors.white),
-                Gaps.v5,
-                Text("Home", style: TextStyle(color: Colors.white)),
-              ],
+            Gaps.h24,
+            NavTab(
+              text: "Inbox",
+              isSelected: _selectedIdx == 3,
+              icon: FontAwesomeIcons.message,
+              selectedIcon: FontAwesomeIcons.solidMessage,
+              onTab: () => _onTap(3),
             ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                FaIcon(FontAwesomeIcons.house, color: Colors.white),
-                Gaps.v5,
-                Text("Home", style: TextStyle(color: Colors.white)),
-              ],
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                FaIcon(FontAwesomeIcons.house, color: Colors.white),
-                Gaps.v5,
-                Text("Home", style: TextStyle(color: Colors.white)),
-              ],
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                FaIcon(FontAwesomeIcons.house, color: Colors.white),
-                Gaps.v5,
-                Text("Home", style: TextStyle(color: Colors.white)),
-              ],
+            NavTab(
+              text: "Profile",
+              isSelected: _selectedIdx == 4,
+              icon: FontAwesomeIcons.user,
+              selectedIcon: FontAwesomeIcons.solidUser,
+              onTab: () => _onTap(4),
             ),
           ],
         ),
--- lib/features/main_navigation/widgets/nav_tab.dart
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -3,17 +3,40 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 
 class NavTab extends StatelessWidget {
-  const NavTab({super.key});
+  const NavTab({
+    super.key,
+    required this.text,
+    required this.isSelected,
+    required this.icon,
+    required this.selectedIcon,
+    required this.onTab,
+  });
+
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final IconData selectedIcon;
+  final Function onTab;
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        FaIcon(FontAwesomeIcons.house, color: Colors.white),
-        Gaps.v5,
-        Text("Home", style: TextStyle(color: Colors.white)),
-      ],
+    return Expanded(
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () => onTab(),
+        child: AnimatedOpacity(
+          opacity: isSelected ? 1 : 0.6,
+          duration: Duration(microseconds: 300),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FaIcon(isSelected ? selectedIcon : icon, color: Colors.white),
+              Gaps.v5,
+              Text(text, style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
