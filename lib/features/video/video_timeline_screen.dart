2ad4a080f73ce725b3035ac6657fc837a4e3bc98
seokgyeong Lee <ho04029@naver.com>
2025-11-03 14:26:49 +0900

    feat: 메인 화면 영상끝났을 때 다음 화면으로 넘기기

--- lib/features/video/video_timeline_screen.dart
+++ lib/features/video/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/video/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -12,20 +13,29 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
 
   final PageController _pageController = PageController();
 
-  List<Color> colors = [Colors.blue, Colors.red, Colors.yellow, Colors.teal];
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
 
-  void _onPageChange(int page) {
-    _pageController.animateTo(
-      page.toDouble(),
-      duration: const Duration(milliseconds: 100),
-      curve: Curves.linear,
+  void _onPageChanged(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
     );
-
     if (page == _itemCount - 1) {
       _itemCount = _itemCount + 4;
-      colors.addAll([Colors.blue, Colors.red, Colors.yellow, Colors.teal]);
       setState(() {});
     }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(duration: _scrollDuration, curve: _scrollCurve);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -33,13 +43,10 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     return PageView.builder(
       controller: _pageController,
       scrollDirection: Axis.vertical,
-      onPageChanged: _onPageChange,
+      onPageChanged: _onPageChanged,
       itemCount: _itemCount,
       itemBuilder:
-          (context, index) => Container(
-            color: colors[index],
-            child: Center(child: Text("screen $index")),
-          ),
+          (context, index) => VideoPost(onVideoFinished: _onVideoFinished),
     );
   }
 }
