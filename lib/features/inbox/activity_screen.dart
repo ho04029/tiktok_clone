aea229a3ad3d222ce4b5d951a16446d9f62627e0
seokgyeong Lee <ho04029@naver.com>
2025-11-20 15:20:41 +0900

    feat: activity화면 애니메이션 추가

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -3,13 +3,59 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
-class ActivityScreen extends StatelessWidget {
+class ActivityScreen extends StatefulWidget {
   const ActivityScreen({super.key});
+
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen>
+    with SingleTickerProviderStateMixin {
+  final List<String> _notifications = List.generate(20, (index) => "${index}h");
+
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 200),
+  );
+
+  late final Animation<double> _animation = Tween(
+    begin: 0.0,
+    end: 0.5,
+  ).animate(_animationController);
+
+  void _onDismissed(String notification) {
+    _notifications.remove(notification);
+    setState(() {});
+  }
+
+  void _onTitleTap() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("activity")),
+      appBar: AppBar(
+        title: GestureDetector(
+          onTap: _onTitleTap,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("activity"),
+              Gaps.h2,
+              RotationTransition(
+                turns: _animation,
+                child: FaIcon(FontAwesomeIcons.chevronDown, size: Sizes.size14),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: ListView(
         children: [
           Gaps.v14,
@@ -24,76 +70,79 @@ class ActivityScreen extends StatelessWidget {
             ),
           ),
           Gaps.v14,
-          Dismissible(
-            key: Key("x"),
-            background: Container(
-              alignment: Alignment.centerLeft,
-              color: Colors.green,
-              child: Padding(
-                padding: EdgeInsets.only(left: Sizes.size10),
-                child: FaIcon(
-                  FontAwesomeIcons.checkDouble,
-                  color: Colors.white,
-                  size: Sizes.size32,
-                ),
-              ),
-            ),
-            secondaryBackground: Container(
-              alignment: Alignment.centerRight,
-              color: Colors.red,
-              child: Padding(
-                padding: EdgeInsets.only(right: Sizes.size10),
-                child: FaIcon(
-                  FontAwesomeIcons.trashCan,
-                  color: Colors.white,
-                  size: Sizes.size32,
-                ),
-              ),
-            ),
-            child: ListTile(
-              leading: Container(
-                width: Sizes.size52,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.grey.shade400,
-                    width: Sizes.size1,
+          for (var notification in _notifications)
+            Dismissible(
+              key: Key(notification),
+              onDismissed: (direction) => _onDismissed(notification),
+              background: Container(
+                alignment: Alignment.centerLeft,
+                color: Colors.green,
+                child: Padding(
+                  padding: EdgeInsets.only(left: Sizes.size10),
+                  child: FaIcon(
+                    FontAwesomeIcons.checkDouble,
+                    color: Colors.white,
+                    size: Sizes.size32,
                   ),
                 ),
-                child: Center(
-                  child: FaIcon(FontAwesomeIcons.bell, color: Colors.black),
+              ),
+              secondaryBackground: Container(
+                alignment: Alignment.centerRight,
+                color: Colors.red,
+                child: Padding(
+                  padding: EdgeInsets.only(right: Sizes.size10),
+                  child: FaIcon(
+                    FontAwesomeIcons.trashCan,
+                    color: Colors.white,
+                    size: Sizes.size32,
+                  ),
                 ),
               ),
-              title: RichText(
-                text: TextSpan(
-                  text: "Account updates:",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                    fontSize: Sizes.size16,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: " Upload longer videos",
-                      style: TextStyle(fontWeight: FontWeight.normal),
+              child: ListTile(
+                minVerticalPadding: Sizes.size16,
+                leading: Container(
+                  width: Sizes.size52,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.grey.shade400,
+                      width: Sizes.size1,
                     ),
-                    TextSpan(
-                      text: " 1h",
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey.shade500,
+                  ),
+                  child: Center(
+                    child: FaIcon(FontAwesomeIcons.bell, color: Colors.black),
+                  ),
+                ),
+                title: RichText(
+                  text: TextSpan(
+                    text: "Account updates:",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                      fontSize: Sizes.size16,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: " Upload longer videos",
+                        style: TextStyle(fontWeight: FontWeight.normal),
                       ),
-                    ),
-                  ],
+                      TextSpan(
+                        text: " $notification",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                trailing: FaIcon(
+                  FontAwesomeIcons.chevronRight,
+                  size: Sizes.size16,
                 ),
               ),
-              trailing: FaIcon(
-                FontAwesomeIcons.chevronRight,
-                size: Sizes.size16,
-              ),
             ),
-          ),
         ],
       ),
     );
