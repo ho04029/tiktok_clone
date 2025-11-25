17ce6910a1fbdee938ad615d2637a9b3a3921cb0
seokgyeong Lee <ho04029@naver.com>
2025-11-25 13:06:45 +0900

    feat: chats 화면

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -12,6 +13,67 @@ class ChatsScreen extends StatefulWidget {
 class _ChatsScreenState extends State<ChatsScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
 
+  final List<int> _items = [];
+
+  final Duration _duration = Duration(milliseconds: 300);
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(_items.length, duration: _duration);
+      _items.add(_items.length);
+    }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) =>
+            Container(color: Colors.red, child: _makeTile(index)),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => ChatDetailScreen()));
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: CircleAvatar(
+        radius: 25,
+        foregroundImage: NetworkImage(
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsv_pZL9XVHMiLKMnV89B1LauRL2t1nis-LeK96R_yOtlAjBF8s1LSpMJHVPoFFrq1wlg&usqp=CAU",
+        ),
+        child: Text("user"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "another ($index)",
+            style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+          Text(
+            "2: 16pm",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size16,
+            ),
+          ),
+        ],
+      ),
+      subtitle: Text("don't forget to make video"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,39 +81,20 @@ class _ChatsScreenState extends State<ChatsScreen> {
         elevation: 1,
         title: Text("DirectMessages"),
         actions: [
-          IconButton(onPressed: () {}, icon: FaIcon(FontAwesomeIcons.plus)),
+          IconButton(onPressed: _addItem, icon: FaIcon(FontAwesomeIcons.plus)),
         ],
       ),
       body: AnimatedList(
         key: _key,
         padding: EdgeInsets.symmetric(vertical: Sizes.size10),
         itemBuilder: (context, index, animation) {
-          return ListTile(
-            leading: CircleAvatar(
-              radius: 25,
-              foregroundImage: NetworkImage(
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsv_pZL9XVHMiLKMnV89B1LauRL2t1nis-LeK96R_yOtlAjBF8s1LSpMJHVPoFFrq1wlg&usqp=CAU",
-              ),
-              child: Text("user"),
+          return FadeTransition(
+            opacity: animation,
+            key: UniqueKey(),
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: _makeTile(index),
             ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  "another ($index)",
-                  style: TextStyle(fontWeight: FontWeight.w600),
-                ),
-                Text(
-                  "2: 16pm",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size16,
-                  ),
-                ),
-              ],
-            ),
-            subtitle: Text("don't forget to make video"),
           );
         },
       ),
