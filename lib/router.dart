ba98934be85f151fe1178a8e9af25ef36a0bcaae
seokgyeong Lee <ho04029@naver.com>
2026-01-16 16:49:32 +0900

    feat: 카메라 패키지 설치

--- lib/router.dart
+++ lib/router.dart
@@ -1,42 +1,8 @@
 import 'package:go_router/go_router.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
-import 'package:tiktok_clone/features/authentication/login_screen.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:tiktok_clone/features/authentication/username_screen.dart';
-import 'package:tiktok_clone/features/users/user_profile_screen.dart';
+import 'package:tiktok_clone/features/video/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
-    GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: (context, state) => const SignUpScreen(),
-      routes: [
-        GoRoute(
-          path: UsernameScreen.routeURL,
-          name: UsernameScreen.routeName,
-          builder: (context, state) => UsernameScreen(),
-        ),
-        GoRoute(
-          path: EmailScreen.routeName,
-          builder: (context, state) {
-            final args = state.extra as EmailScreenArgs;
-            return EmailScreen(username: args.username);
-          },
-        ),
-      ],
-    ),
-    GoRoute(
-      path: LoginScreen.routeName,
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: "/users/:username",
-      builder: (context, state) {
-        final username = state.params['username'];
-        final tab = state.queryParams['show'];
-        return UserProfileScreen(username: username!, tab: tab!);
-      },
-    ),
+    GoRoute(path: "/", builder: (context, state) => VideoRecordingScreen()),
   ],
 );
