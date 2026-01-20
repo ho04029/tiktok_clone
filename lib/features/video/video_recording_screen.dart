d536f0c560c338d105282653ee124b4b056ccfb6
seokgyeong Lee <ho04029@naver.com>
2026-01-20 14:30:27 +0900

    feat: 카메라

--- lib/features/video/video_recording_screen.dart
+++ lib/features/video/video_recording_screen.dart
@@ -1,6 +1,8 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -12,8 +14,26 @@ class VideoRecordingScreen extends StatefulWidget {
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   bool _hasPermission = false;
 
+  bool _isSelfieMode = false;
+
+  late CameraController _cameraController;
+
   Future<void> initCamera() async {
-    await availableCameras();
+    final cameras = await availableCameras();
+
+    // 사용자의 하드웨어에 카메라가 있는지 없는지 확인
+    if (cameras.isEmpty) {
+      return;
+    }
+
+    // cameras[0]은 후면 카메라를 의미
+    _cameraController = CameraController(
+      cameras[_isSelfieMode ? 1 : 0],
+      ResolutionPreset.ultraHigh,
+    );
+
+    // 카메라 컨트롤러 초기화
+    await _cameraController.initialize();
   }
 
   Future<void> initPermissions() async {
@@ -37,8 +57,52 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     initPermissions();
   }
 
+  // 비동기함수로 사용할 예정
+  Future<void> _toggleSelfieMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    await initCamera();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black, body: Container());
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child:
+            !_hasPermission || !_cameraController.value.isInitialized
+                ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Initializing...",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                    Gaps.v20,
+                    CircularProgressIndicator.adaptive(),
+                  ],
+                )
+                : Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CameraPreview(_cameraController),
+                    Positioned(
+                      top: Sizes.size20,
+                      left: Sizes.size20,
+                      child: IconButton(
+                        color: Colors.white,
+                        onPressed: _toggleSelfieMode,
+                        icon: const Icon(Icons.cameraswitch),
+                      ),
+                    ),
+                  ],
+                ),
+      ),
+    );
   }
 }
