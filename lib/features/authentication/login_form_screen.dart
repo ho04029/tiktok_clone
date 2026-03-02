b118f1be35480fd1d1690d926365bb6fe0de3fcc
seokgyeong Lee <ho04029@naver.com>
2026-03-02 17:05:03 +0900

    feat: 이메일, 비밀번호로 로그인하기

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/view_models/login_view_model.dart';
 import 'package:tiktok_clone/features/authentication/widgets/from_button.dart';
 import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
-class LoginFormScreen extends StatefulWidget {
+class LoginFormScreen extends ConsumerStatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
-  State<LoginFormScreen> createState() => _LoginFormScreenState();
+  ConsumerState<LoginFormScreen> createState() => _LoginFormScreenState();
 }
 
-class _LoginFormScreenState extends State<LoginFormScreen> {
+class _LoginFormScreenState extends ConsumerState<LoginFormScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   Map<String, String> formData = {};
 
@@ -21,6 +23,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
 
+        ref
+            .read(loginProvider.notifier)
+            .login(formData["email"]!, formData["password"]!, context);
         // context.goNamed(InterestsScreen.routeName);
       }
     }
@@ -59,7 +64,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
               Gaps.v28,
               GestureDetector(
                 onTap: _onSubmitTap,
-                child: FormButton(disabled: false, text: "Log in"),
+                child: FormButton(
+                    disabled: ref.watch(loginProvider).isLoading,
+                    text: "Log in"),
               ),
             ],
           ),
--- lib/features/authentication/view_models/login_view_model.dart
+++ lib/features/authentication/view_models/login_view_model.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
+import 'package:tiktok_clone/utils.dart';
 
 class LoginViewModel extends AsyncNotifier {
   late final AuthenticationRepo _repository;
@@ -11,13 +14,19 @@ class LoginViewModel extends AsyncNotifier {
     _repository = ref.read(authRepo);
   }
 
-  Future<void> login(String email, String password) async {
+  Future<void> login(
+      String email, String password, BuildContext context) async {
     state = AsyncValue.loading();
     state = await AsyncValue.guard(
         () async => await _repository.signIn(email, password));
+    if (state.hasError) {
+      showFirebaseErrorSnack(context, state.error);
+    } else {
+      context.go("/home");
+    }
   }
 }
 
-final LoginProvider = AsyncNotifierProvider<LoginViewModel, void>(
+final loginProvider = AsyncNotifierProvider<LoginViewModel, void>(
   () => LoginViewModel(),
 );
