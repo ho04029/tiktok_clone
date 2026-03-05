3cebf6674286161f9164f80f8d0506b263684b7c
seokgyeong Lee <ho04029@naver.com>
2026-03-05 16:04:34 +0900

    feat: 유저 프로필

---
+++ lib/features/users/view_models/user_view_model.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/users/models/user_profile_model.dart';
+
+class UserViewModel extends AsyncNotifier<UserProfileModel> {
+  @override
+  FutureOr<UserProfileModel> build() {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+final userProvider = AsyncNotifierProvider(() => UserViewModel());
