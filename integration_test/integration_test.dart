c0756eb22f1a76b071085e829794ff8072089f72
seokgyeong Lee <ho04029@naver.com>
2026-05-04 14:56:05 +0900

    test: 통합테스트

---
+++ integration_test/integration_test.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:tiktok_clone/firebase_options.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  });
+}
